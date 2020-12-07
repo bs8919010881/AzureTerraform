124876225ae9e93a7af625b96bb6ae169df54336
bs8919010881 <shasidc@gmail.com>
2020-12-07 16:00:32 +0530

    Base Repo commit

--- Provider.tf
+++ Provider.tf
@@ -1,12 +1,15 @@
+provider "aurerm"{
+    subscription_id = "5e5e6682-50c9-477e-a023-b5cb1a75cb39"
+    client_id       = "c555170d-7054-4bab-9974-e0dd02f42f32"
+    tenant_id       = "2d6f3630-8651-4156-9537-9d21c05497d2"
+}
 variable "prefix" {
   default = "tfvmex"
 }
-
 resource "azurerm_resource_group" "main" {
   name     = "${var.prefix}-resources"
   location = "West US 2"
 }
-
 resource "azurerm_virtual_network" "main" {
   name                = "${var.prefix}-network"
   address_space       = ["10.0.0.0/16"]
