55ab04c7a011b0aaa7b8a16ea19b5c51068c4142
bs8919010881 <shasidc@gmail.com>
2020-12-07 15:26:17 +0530

    Base Repo commit

--- Provider.tf
+++ Provider.tf
@@ -1,6 +1,34 @@
-provider "azurerm" {
- 
- subscription_id = "5e5e6682-50c9-477e-a023-b5cb1a75cb39"
-  client_id       = "c555170d-7054-4bab-9974-e0dd02f42f32"
-  tenant_id       = "2d6f3630-8651-4156-9537-9d21c05497d2"
+variable "prefix" {
+  default = "tfvmex"
+}
+
+resource "azurerm_resource_group" "main" {
+  name     = "${var.prefix}-resources"
+  location = "West US 2"
+}
+
+resource "azurerm_virtual_network" "main" {
+  name                = "${var.prefix}-network"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+}
+
+resource "azurerm_subnet" "internal" {
+  name                 = "internal"
+  resource_group_name  = azurerm_resource_group.main.name
+  virtual_network_name = azurerm_virtual_network.main.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
+
+resource "azurerm_network_interface" "main" {
+  name                = "${var.prefix}-nic"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+
+  ip_configuration {
+    name                          = "testconfiguration1"
+    subnet_id                     = azurerm_subnet.internal.id
+    private_ip_address_allocation = "Dynamic"
+  }
 }
