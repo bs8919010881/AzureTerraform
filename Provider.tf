26aeba2be5d850eb2153c1e9bd7c7ddc38dba484
bs8919010881 <shasidc@gmail.com>
2020-12-07 14:59:10 +0530

    Base Repo commit

---
+++ Provider.tf
@@ -0,0 +1,6 @@
+provider "azurerm" {
+ 
+ subscription_id = "5e5e6682-50c9-477e-a023-b5cb1a75cb39"
+  client_id       = "c555170d-7054-4bab-9974-e0dd02f42f32"
+  tenant_id       = "2d6f3630-8651-4156-9537-9d21c05497d2"
+}
